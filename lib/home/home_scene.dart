2559458bab5bee96599baf1a03cdb10798886011
Victor903 <1215702617@qq.com>
2019-02-18 18:21:00 +0800

    Merge https://github.com/Victor903/flutter_mzshu
    # Conflicts:
    #	README.md

--- lib/home/home_scene.dart
+++ lib/home/home_scene.dart
@@ -13,36 +13,64 @@ class HomeSceneState extends State<HomeScene> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4,
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           brightness: Brightness.light,
-          title: Container(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: TabBar(
-              labelColor: SQColor.darkGray,
-              labelStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              unselectedLabelColor: SQColor.gray,
-              indicatorColor: SQColor.secondary,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorWeight: 3,
-              indicatorPadding: EdgeInsets.fromLTRB(8, 0, 8, 5),
-              tabs: [
-                Tab(text: '精选'),
-                Tab(text: '女生'),
-                Tab(text: '男生'),
-                Tab(text: '漫画'),
-              ],
+          leading: Container(
+           width: 35,
+           height: 35,
+           padding: EdgeInsets.symmetric(horizontal: 12),
+           child:  Image.asset("img/logo.png",),
+          ),
+          title:
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 0),
+              height: 30,
+              width: 210,
+              margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+              decoration: new BoxDecoration(
+                border: new Border.all(color: SQColor.white, width: 1.5), // 边色与边宽度
+                color: SQColor.orange, // 底色
+                borderRadius: new BorderRadius.circular((30.0)), // 圆角度
+              ),
+              child: TabBar(
+                labelColor: SQColor.orange,
+                labelStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+                unselectedLabelColor: SQColor.white,
+                indicator: new BoxDecoration(
+                  border: new Border.all(color: SQColor.white, width: 0), // 边色与边宽度
+                   color: SQColor.white, // 底色
+                   borderRadius: new BorderRadius.circular((30.0)), // 圆角度
+                ),
+                indicatorColor: SQColor.white,
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicatorWeight: 30,
+                //indicatorPadding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                tabs: [
+                  Tab(text: '精选'),
+                  Tab(text: '女生'),
+                  Tab(text: '男生'),
+                 // Tab(text: '漫画'),
+                ],
+              ),
+          ),
+          actions: <Widget>[
+        Container(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+           child: IconButton(icon: Icon(Icons.search,size: 35,color: SQColor.white,)
+                , onPressed: ()=>Toast.show("search")
             ),
           ),
-          backgroundColor: SQColor.white,
+          ],
+          backgroundColor: SQColor.orange,
           elevation: 0,
         ),
         body: TabBarView(children: [
           HomeListView(HomeListType.excellent),
           HomeListView(HomeListType.female),
           HomeListView(HomeListType.male),
-          HomeListView(HomeListType.cartoon),
+         // HomeListView(HomeListType.cartoon),
         ]),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:flutter/services.dart';
-
 import 'package:mzshu/app/app_scene.dart';
+import 'package:mzshu/app_welcome.dart';
 
 void main() {
-  runApp(AppScene());
+  runApp(MyApp());
   //以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，
   // 是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
   if (Platform.isAndroid) {
     SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(statusBarColor: Colors.transparent);
     SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+
+}
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'MzshuWelcome',
+        routes: <String,WidgetBuilder>{//配置路径
+          '/HomePage':(BuildContext context)  => new AppScene(),
+        },
+        theme: new ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+          // counter didn't reset back to zero; the application is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: new SplashPage()
+    );
   }
 }
